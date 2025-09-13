32f2f869e5f454d7c08cef9e90aa10138b71c0d2
Vivek <vivekghule777@gmail.com>
2025-09-13 22:44:50 +0530

    add some routes

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const app = express();
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const mongoDB = require('./config/mongoose-connection')
-const ownersRouter = require('./routes/ownersRouter')
-const usersRouter = require('./routes/usersRouter')
-const productsRouter = require('./routes/productsRouter')
+const ownersRouter = require('./routes/owner.router')
+const usersRouter = require('./routes/users.router')
+const productsRouter = require('./routes/products.router')
+const indexRouter = require('./routes/index')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+require('dotenv').config();
 
 app.set("view engine","ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")))
+app.use(expressSession({
+    resave:false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+
+}))
+app.use(flash())
 
 
 // Routes
 
-app.use("/owners",ownersRouter);
-app.use("/users",usersRouter);
-app.use("/products",productsRouter);
+app.use("/",indexRouter);
+app.use("/api/v1/owners",ownersRouter);
+app.use("/api/v1/users",usersRouter);
+app.use("/api/v1/products",productsRouter);
 
 app.listen(8080)
